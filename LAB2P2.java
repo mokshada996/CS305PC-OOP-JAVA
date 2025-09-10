535c29b28c809c64cc58f6035efeee48dfeea8e8
mokshada996 <vkbmokshadavangmai@gmail.com>
2025-09-10 11:18:53 +0530

    Add files via upload

---
+++ LAB2P2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class LAB2P2{
+    public static void main(String...args){
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();
+        int y=x;
+        
+        System.out.println("output is: "+ x++ + ++x);
+        x=y;
+        System.out.println("output is: "+ (x++ + ++x));
+        x=y;
+        System.out.println(x++ + ++x +" :output is ");
+    }
+}
