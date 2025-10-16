fb863359b368ef9fc247f70172f0ff264133f1ff
mokshada996 <vkbmokshadavangmai@gmail.com>
2025-10-16 18:39:56 +0530

    Delete LAB4 directory

--- LAB4/Operation.java
+++
@@ -1,6 +0,0 @@
-package Student;
-interface Operation{
-	abstract void registration(Student std);
-	abstract void logIn(String id,String password);
-	abstract void exit();
-}
--- LAB4/Person.java
+++
@@ -1,8 +0,0 @@
-package Student;
-abstract class Person{
-	protected String name;
-	Person(String name){
-		this.name=name;		
-	}
-	abstract String getDetails();
-} 
