93dd42417eed875df877b5a39d8b258f60dec764
mokshada996 <vkbmokshadavangmai@gmail.com>
2025-10-29 12:04:54 +0530

    Add files via upload

---
+++ AnnoDemo.java
@@ -0,0 +1,37 @@
+class AnnoP{
+	@Deprecated
+	public  void depM(){
+		System.out.println("Depricated method");
+	}
+	public void ovR(){
+		System.out.println("AnnoP");
+	}
+}
+class AnnoC extends AnnoP{
+	@Override
+	public void ovR(){
+		System.out.println("AnnoC");
+	}
+}
+public class AnnoDemo{
+	@SuppressWarnings("deprecation")
+	public static void main(String...args){
+		AnnoC ac=  new AnnoC();
+		ac.ovR();
+		ac.depM();
+	}
+}		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
